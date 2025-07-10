976c258f935a227c7e0f291ac33e198103733c60
its-kronos <anseld41@gmail.com>
2025-07-09 21:10:14 -0400

    restructure to remove flask, which was an unecessary module!

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route("/api/test")
-def test():
-    return jsonify({
-        "test":"Truefun"
-    })
--- frontend/src/pages/api/test.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req,res){
-    const response = await fetch("http://localhost:5000/api/test");
-    const data = await response.json();
-    res.status(200).json(data);
-}
